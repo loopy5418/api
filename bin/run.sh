006e6e267d536a10ac1ce37143f18e793cf8c196
Loopy5418 <73636770+loopy5418@users.noreply.github.com>
2025-04-30 19:07:53 +0300

    Update run.sh
    fix

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")/.."
-gunicorn wsgi:app --bind 0.0.0.0:$PORT --log-level=debug --workers=4 &
-python dbot/bot.py
+
+gunicorn wsgi:app --bind 0.0.0.0:$PORT --log-level=debug --workers=4

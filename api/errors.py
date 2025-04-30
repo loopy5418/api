39806a6c9f7b260f6067cdf0a3232f99d642b751
Loopy5418 <73636770+loopy5418@users.noreply.github.com>
2025-04-30 16:32:52 +0300

    Update errors.py
    change error response

--- api/errors.py
+++ api/errors.py
@@ -5,4 +5,4 @@ errors = Blueprint("errors", __name__)
 
 @errors.app_errorhandler(Exception)
 def server_error(error):
-    return Response(f"Oops, got an error! {error}", status=500)
+    return Response(f"{error} (error)", status=500)

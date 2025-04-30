5fa0090be56934720560c1ef61aca48508ed800e
Loopy5418 <73636770+loopy5418@users.noreply.github.com>
2025-04-30 18:59:25 +0300

    Update run.sh
    add bot run

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
-gunicorn wsgi:app --bind 0.0.0.0:$PORT --log-level=debug --workers=4
+cd "$(dirname "$0")/.."
+gunicorn wsgi:app --bind 0.0.0.0:$PORT --log-level=debug --workers=4 &
+python dbot/bot.py

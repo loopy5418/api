e08d2e7fff749fa0ffc438b10335028ee984e68e
Loopy5418 <73636770+loopy5418@users.noreply.github.com>
2025-05-01 13:28:13 +0300

    Create bot.py

---
+++ dbot/bot.py
@@ -0,0 +1,20 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True  # Required for reading message content
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run(TOKEN)
